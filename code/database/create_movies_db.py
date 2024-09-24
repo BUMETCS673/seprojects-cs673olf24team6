7f0d424c185621bd5e41865712f6783c39dae3dd
Elizabeth Tyree <elityree@bu.edu>
2024-09-23 21:31:06 -0400

    adding database files

---
+++ code/database/create_movies_db.py
@@ -0,0 +1,20 @@
+# Code taken from: SQLite Python: Creating a New Database
+# Reference: https://www.sqlitetutorial.net/sqlite-python/creating-database/
+
+import sqlite3
+
+def create_sqlite_database(filename):
+    """ create a database connection to an SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(filename)
+        print(sqlite3.sqlite_version)
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_sqlite_database("movies.db")
