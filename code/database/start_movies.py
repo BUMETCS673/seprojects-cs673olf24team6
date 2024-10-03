5bcf1a1c07e2d8d5cbd94753e4b272e3ef6a70db
Elizabeth Tyree <elityree@bu.edu>
2024-10-02 20:37:05 -0400

    Database docker file

--- code/database/start_movies.py
+++ code/database/start_movies.py
@@ -2,11 +2,13 @@
 create the movies.db SQLite database, creates the tables,
 and then loads the tables with data."""
 
-exec(open("create_movies_db.py").read())
+exec(open("create_movies_db.py").read()) # Creates the database
 
-exec(open("create_movies_db_tables.py").read())
+exec(open("create_movies_db_tables.py").read()) # Creates tables
 
 exec(open("load_movies_db_data.py").read())
 
 exec(open("db_test_data.py").read())
+
+
 
