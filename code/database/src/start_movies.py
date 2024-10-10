d7b81b682659ce70d515a94581e6738a5d88dce6
Joshua Shilts <jshilts@bu.edu>
2024-10-10 13:39:55 -0400

    dev: changing pathway so will not conflict with flask container when mounting

--- code/database/src/start_movies.py
+++ code/database/src/start_movies.py
@@ -3,14 +3,14 @@ import os
 create the movies.db SQLite database, creates the tables,
 and then loads the tables with data."""
 
-exec(open("/app/src/create_movies_db.py").read()) # Creates the database
+exec(open("/database/src/create_movies_db.py").read()) # Creates the database
 
-exec(open("/app/src/create_movies_db_tables.py").read()) # Creates tables
+exec(open("/database/src/create_movies_db_tables.py").read()) # Creates tables
 
-exec(open("/app/src/load_movies_db_data.py").read())
+exec(open("/database/src/load_movies_db_data.py").read())
 
 # Path to the file
-file_path = "/app/src/movies.db"
+file_path = "/database/src/movies.db"
 
 # Octal permission: e.g., 0o777 gives full permissions to the owner, group, and others
 new_permissions = 0o777
