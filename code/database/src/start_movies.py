1e0c9b396af0923c8eeaa33060e9f8e5be72ed67
Joshua Shilts <jshilts@bu.edu>
2024-10-10 13:06:29 -0400

    dev: trying to change db permission

--- code/database/src/start_movies.py
+++ code/database/src/start_movies.py
@@ -1,3 +1,4 @@
+import os
 """This start_movies.py script runs all the files needed to
 create the movies.db SQLite database, creates the tables,
 and then loads the tables with data."""
@@ -7,3 +8,14 @@ exec(open("/app/src/create_movies_db.py").read()) # Creates the database
 exec(open("/app/src/create_movies_db_tables.py").read()) # Creates tables
 
 exec(open("/app/src/load_movies_db_data.py").read())
+
+# Path to the file
+file_path = "/app/src/movies.db"
+
+# Octal permission: e.g., 0o777 gives full permissions to the owner, group, and others
+new_permissions = 0o777
+
+# Change the permissions of the file
+os.chmod(file_path, new_permissions)
+
+print(f"Permissions for {file_path} have been changed.")
